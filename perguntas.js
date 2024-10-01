d8b95f65f8a656451b2131fc1a1e146412c66fce
bolhasss <128045911+bolhasss@users.noreply.github.com>
2024-10-01 08:18:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,99 @@
+         criaCartao (
+        'Filosofia',
+        'Quem criou o marxismo',
+        'Karl Marx')
+        
+        criaCartao (
+        'História',
+        'Quem descobriu o Brasil',
+        'Pedro Álvares Cabral')
+        
+        criaCartao (
+        'Arte',
+        'Quem criou a escultura de David na igreja da Itália',
+        'Michelangelo')
+        
+        criaCartao (
+        'Matemática',
+        'Qual a fórmula da razao',
+        'Numerador/Denominador')
+        
+        criaCartao (
+        'Arte',
+        'Quem criou a Mona Lisa',
+        'Leonardo Da Vinci')
+        
+        criaCartao (
+        'História',
+        'Aonde começou o fascismo',
+        'Itália')
+        
+        criaCartao (
+        'História',
+        'Qual era os pilar principal para a ideologia do nazismo',
+        'antissemitismo(ódio de judeus)')
+        
+        criaCartao (
+        'Biologia',
+        'O que estuda as células',
+        'Citologia')
+        
+        criaCartao (
+        'Arte',
+        'Porque Van Gogh cortou sua própria orelha',
+        'Briga em bar enquanto estava muito bravo')
+        
+        criaCartao (
+        'Filosofia',
+        'O que é ética',
+        'princípios que orientam o comportamento humano, determinando o que é certo e errado')
+        
+        criaCartao (
+        'História',
+        'Quando começou e terminou a Segunda Guerra Mundial',
+        '1939-1945')
+        
+        criaCartao (
+        'Filosofia',
+        'O que foi o período socrático',
+        'estudo da ética, política e conhecimento')
+        
+        criaCartao (
+        'Arte',
+        'Qual artista criou o primeiro rascunho de um helicóptero',
+        'Leonardo Da Vinci')
+        
+        criaCartao (
+        'Filosofia',
+        'Qual conceito Aristóteles é conhecido por',
+        'Que a felicidade é o fim da vida humana, alcançada pela virtude')
+        
+        criaCartao (
+        'História',
+        'Quantos anos demorou a Primeira Guerra Mundial',
+        '4 anos, de 1914-1918')
+        
+        criaCartao (
+        'História',
+        'Antes da Revoluçao Russa, ela era conhecida por ser um país',
+        'Agrário, feudal, dominada pelo clero e por imperadores')
+        
+        criaCartao (
+        'Arte',
+        'Quem pintou “The Last Supper”',
+        'Leonardo Da Vinci')
+        
+        criaCartao (
+        'Biologia',
+        'Quantos cromossomos tem uma pessoa',
+        '46')
+        
+        criaCartao (
+        'Física',
+        'Qual a medida de comprimento em S.I.',
+        'Centímetro')
+        
+        criaCartao (
+        'Biologia',
+        'Quantos cromossomos tem uma pessoa com síndrome de down',
+        '42')
